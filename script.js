6f122117e774d24bf07ff8e4afd411ebdb0fb7cd
Arthur Gomes <arthurcosta007110@gmail.com>
2021-07-11 20:15:51 -0300

    Adicionado Colod Picker

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
             window.saveAs(blob, "carta.png");
         });
     });
+
+    $("#carta").css('border', '2px solid #000');
+    $("#imagem-carta-container").css('border', '2px solid #000');
+    $("#descricao-carta").css('border', '2px solid #000');
+    $("#custo-mana-carta").css('background-color', "#000");
+
+    $("#cor-texto-carta-input").val("#ffffff");
 });
 
 $("#nome-carta-input").blur(function(){
@@ -26,4 +33,22 @@ $("#textura-carta-input").on('change', function() {
     $("#carta").removeClass();
     $("#carta").addClass("carta "+this.value);
 });
-
+/*$("#cor-carta-input").ColorPicker(function(){
+    //2px solid white
+    $("#carta").css('border', '2px solid '+this.val());
+    $("#nome-carta").css('color', this.val());
+});*/
+$("#cor-carta-input").change(function(){
+    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
+    $("#carta").css('border', '2px solid '+$(this).val());
+    $("#imagem-carta-container").css('border', '2px solid '+$(this).val());
+    $("#descricao-carta").css('border', '2px solid '+$(this).val());
+    $("#custo-mana-carta").css('background-color', $(this).val());
+});
+$("#cor-texto-carta-input").change(function(){
+    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
+    $("#nome-carta").css('color', $(this).val());
+    $("#custo-mana-carta").css('color', $(this).val());
+    $("#descricao-carta").css('color', $(this).val());
+    $("#atributos-carta").css('color', $(this).val());
+});
