a7800daa18a1b10064540e404540266903ddcf17
Arthur Gomes <arthurcosta007110@gmail.com>
2021-07-11 19:33:13 -0300

    Melhorias do Sistema

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ $(document).ready(function(){
         domtoimage.toBlob(document.getElementById("carta"))
         .then(function(blob){
             window.saveAs(blob, "carta.png");
-        })
-    })
-})
+        });
+    });
+});
+
+$("#nome-carta-input").blur(function(){
+    $("#nome-carta").html($(this).val());
+});
+$("#custo-mana-carta-input").blur(function(){
+    $("#custo-mana-carta").html($(this).val());
+});
+$("#imagem-url-carta-input").blur(function(){
+    $("#imagem-carta").attr("src", $(this).val());
+});
+$("#descricao-carta-input").blur(function(){
+    $("#descricao-carta").html($(this).val());
+});
+$("#atributos-carta-input").blur(function(){
+    $("#atributos-carta").html($(this).val());
+});
+$("#textura-carta-input").on('change', function() {
+    $("#carta").removeClass();
+    $("#carta").addClass("carta "+this.value);
+});
+
