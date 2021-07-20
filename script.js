38d3db21786e77b2508813f618020b9a1c8e0207
Arthur Gomes <arthurcosta007110@gmail.com>
2021-07-20 02:51:05 -0300

    melhorias da interface
    melhorias na interface, implementacao do gradiente radial, otimizacoes na UI, correcoes de bugs

--- script.js
+++ script.js
@@ -1,85 +1,89 @@
+// Função executada quando a página html é totalmente carregada
 $(document).ready(function(){
-    $("#button").click(function(){
+    //Adicionando uma função ao botão de download
+    $("#download-button").click(function(){
+        //Passando o elemento com id = "carta" para o metodo que transforma o mesmo em imagem
         domtoimage.toBlob(document.getElementById("carta"))
         .then(function(blob){
+            //Fazer download da imagem com extenção ".png"
             window.saveAs(blob, "carta.png");
         });
     });
-
+    //Definindo um padrão de cores na carta mais agradável para o usuário
+    //#region Padrão da Carta Inicial
     $("#carta").css('border', '2px solid #000');
     $("#imagem-carta-container").css('border', '2px solid #000');
     $("#descricao-carta").css('border', '2px solid #000');
     $("#custo-mana-carta").css('background-color', "#000");
-
     $("#cor-texto-carta-input").val("#ffffff");
+    //#endregion
 });
-
 //#region Inputs
-$("#nome-carta-input").blur(function(){
+//Mudando nome da carta a cada input do usuário
+$("#nome-carta-input").on("input", function(){
     $("#nome-carta").html($(this).val());
 });
-$("#custo-mana-carta-input").blur(function(){
+//Mudando custo de mana da carta a cada input do usuário
+$("#custo-mana-carta-input").on("input", function(){
     if($(this).val() < 100 && $(this).val() > 0){
         $("#custo-mana-carta").html($(this).val());
     }else if($(this).val() >= 100){
+        $(this).val(99);
         $("#custo-mana-carta").html("99");
     }else{
+        $(this).val(0);
         $("#custo-mana-carta").html("0");
     }
 });
+//Mudando imagem da carta somente quando o usuário desfoca o input
 $("#imagem-url-carta-input").blur(function(){
     $("#imagem-carta").attr("src", $(this).val());
 });
-$("#descricao-carta-input").blur(function(){
+//Mudando descrição da carta a cada input do usuário
+$("#descricao-carta-input").on("input", function(){
     $("#descricao-carta").html($(this).val());
 });
-$("#atributos-carta-input").blur(function(){
+//Mudando os atributos da carta a cada input do usuário
+$("#atributos-carta-input").on("input", function(){
     $("#atributos-carta").html($(this).val());
 });
+//Aplicando textura a carta
 $("#textura-carta-input").on('change', function() {
     if(this.value == "texura-cor"){
-        $("#cor-fundo-carta-input").attr("hidden", false);
+        $("#cor-fundo-carta-input").show();
     }else{
-        $("#cor-fundo-carta-input").attr("hidden", true);
+        $("#cor-fundo-carta-input").hide();
     }
 
     if(this.value == "texura-gradiente"){
-        $("#gradiente-container").attr("hidden", false);
+        $("#gradiente-container").show();
     }else{
-        $("#gradiente-container").attr("hidden", true);
+        $("#gradiente-container").hide();
         $("#carta").css("background", "");
     }
 
     $("#carta").removeClass();
     $("#carta").addClass("carta "+this.value);
 });
-//#endregion Inputs
-/*$("#cor-carta-input").ColorPicker(function(){
-    //2px solid white
-    $("#carta").css('border', '2px solid '+this.val());
-    $("#nome-carta").css('color', this.val());
-});*/
 $("#cor-carta-input").change(function(){
-    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
     $("#carta").css('border', '2px solid '+$(this).val());
     $("#imagem-carta-container").css('border', '2px solid '+$(this).val());
     $("#descricao-carta").css('border', '2px solid '+$(this).val());
     $("#custo-mana-carta").css('background-color', $(this).val());
 });
 $("#cor-texto-carta-input").change(function(){
-    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
     $("#nome-carta").css('color', $(this).val());
     $("#custo-mana-carta").css('color', $(this).val());
     $("#descricao-carta").css('color', $(this).val());
     $("#atributos-carta").css('color', $(this).val());
 });
 $("#cor-fundo-carta-input").change(function(){
-    //$("#nome-carta").css('background', $(this).val()); custo-mana-carta
     $("#carta").css('background-color', $(this).val());
 });
+//#endregion Inputs
 
 $(document).on("click", ".add", function(){
-    $(this).before("<input type='color' class='gradient-picker' id='gradient-picker'/>"); 
+    $("#cursor-add-cor").before("<input type='color' class='gradient-picker'/>"); 
 
     console.log($(".gradient-picker").length);
     if($(".gradient-picker").length >= 8){
@@ -95,8 +99,8 @@ $("#reset-gradient-button").click(function(){
         $(element).remove();
     });
 
-    $(".add").before("<input type='color' class='gradient-picker' id='gradient-picker'/>");
-    $(".add").before("<input type='color' class='gradient-picker' id='gradient-picker'/>");
+    $("#cursor-add-cor").before("<input type='color' class='gradient-picker' id='gradient-picker'/>");
+    $("#cursor-add-cor").before("<input type='color' class='gradient-picker' id='gradient-picker'/>");
 
     $(".add").show();
     $("#reset-gradient-button").hide();
@@ -112,9 +116,13 @@ $("#gradient-button").click(function(){
         colors += ", " + $(element).val();
     });
 
-    let query = "linear-gradient(to right" + colors + ")";
-    
-    console.log(query);
+    let query = "";
+
+    if($("#tipo-gradiente-input").val() == "linear"){
+        query += "linear-gradient(to right" + colors + ")";
+    }else if($("#tipo-gradiente-input").val() == "radial"){
+        query += "radial-gradient(circle" + colors + ")";
+    }
 
     $("#carta").css("background-image", query);
 
